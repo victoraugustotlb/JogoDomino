622e636804aac52d410a0375aadb72285f045023
Jpl-0 <joaopedrosilva07654@gmail.com>
2026-08-13 11:38:42 -0300

    Update project header and maintain existing code

--- Jogo_Domino/Dom_JLRV_Model.cpp
+++ Jogo_Domino/Dom_JLRV_Model.cpp
@@ -1,9 +1,9 @@
-//Dom_JLRV_PROJETO - Projeto Domino
-//12/08/2026
-//Joao
+//Dom_JLRV_MODEL - Projeto Domino
+//13/08/2026
+//Joao Pedro Lemos Romano Francisco da Silva
 //Leopoldo Ortuzal Zuchieri
 //Renato Corral Silva
-
+//Victor Augusto Toledo Lucio Borghi
 
 
 // Struct dos lados do domino
