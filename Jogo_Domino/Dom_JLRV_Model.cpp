610099834cda340779dbdbc7d5e1cbc4f6df2cdf
Jpl-0 <joaopedrosilva07654@gmail.com>
2026-08-13 12:19:57 -0300

    Update Dom_JLRV_Model.cpp

--- Jogo_Domino/Dom_JLRV_Model.cpp
+++ Jogo_Domino/Dom_JLRV_Model.cpp
@@ -1,9 +1,6 @@
-//Dom_JLRV_MODEL - Projeto Domino
-//13/08/2026
-//Joao Pedro Lemos Romano Francisco da Silva
 //Leopoldo Ortuzal Zuchieri
 //Renato Corral Silva
-//Victor Augusto Toledo Lucio Borghi
+
 
 
 // Struct dos lados do domino
