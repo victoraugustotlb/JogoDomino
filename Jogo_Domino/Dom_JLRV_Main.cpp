d7c601297204cbb2ecd4da297e0a2bc7807b4ee4
LEOPOLDO ORTUZAL ZUCHIERI <ra00370580@pucsp.edu.br>
2026-08-13 11:24:33 -0300

    feat: Cabecalho adicionado ao arquivo Dom_JLRV_Main.cpp seguindo os criterios desejados

--- Jogo_Domino/Dom_JLRV_Main.cpp
+++ Jogo_Domino/Dom_JLRV_Main.cpp
@@ -1,4 +1,11 @@
-#include <stdio.h>
+//Dom_JLRV_PROJETO - Projeto Domino
+//13/08/2026
+//Joao Pedro Lemos Romano Francisco da Silva
+//Leopoldo Ortuzal Zuchieri
+//Renato Corral Silva
+//Victor Augusto Toledo Lucio Borghi
+
+
 #include "Dom_JLRV_Model.cpp"
 #include "Dom_JLVR_View.cpp"
 
