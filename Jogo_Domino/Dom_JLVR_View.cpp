8f18ef44704a5520d9891765a87b9f6f98951459
Victor Augusto Borghi <victoraugustotlb@gmail.com>
2026-08-18 15:39:41 -0300

    Refactor Dom_JLVR_View: add sorting function
    Updated the file by adding a new function to sort pieces and removed the print function.

--- Jogo_Domino/Dom_JLVR_View.cpp
+++ Jogo_Domino/Dom_JLVR_View.cpp
@@ -6,12 +6,9 @@
 //Victor Augusto Toledo Lucio Borghi
 
 #include <stdio.h>
-#include "Dom_JLVR_View.h"
 
-
-void printarPecas(pecas conjunto[]){
-	for (int gerar = 0; gerar < 28; gerar++){
-		printf("[%d|%d]", conjunto[gerar].lado1, conjunto[gerar].lado2);
-	}
+void ordenarPecas(){
+    for (int gerar = 0; gerar < 28; gerar++){
+        printf("[%d|%d]", conjunto[gerar].lado1, conjunto[gerar].lado2);
+    }
 }
-
