ec1b34c9d61ca0ace43cc53473bb1697f6fee301
Victor Augusto Borghi <victoraugustotlb@gmail.com>
2026-08-18 15:40:13 -0300

    Remove comments and clean up includes in Dom_JLRV_Controller

--- Jogo_Domino/Dom_JLRV_Controller.cpp
+++ Jogo_Domino/Dom_JLRV_Controller.cpp
@@ -5,19 +5,14 @@
 //Renato Corral Silva
 //Victor Augusto Toledo Lucio Borghi
 
-#include "Dom_JLRV_Controller.h"
-#include "Dom_JLRV_Model.cpp"
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void embaralharConjunto(peca conjunto[]){
-	
-	for(int i = 27; i > 0; i--){ 
+    for(int i = 27; i > 0; i--){ 
         int j = rand() % (i + 1); 
         peca temp = conjunto[i]; 
         conjunto[i] = conjunto[j]; 
         conjunto[j] = temp; 
     } 
- 
 }
