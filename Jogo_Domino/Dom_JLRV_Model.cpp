073d14cf4a315452f813bfc9f05f467bdf556f7c
João P.  L. <joaopedrosilva07654@gmail.com>
2026-08-20 11:31:08 -0300

    Implement piece generation and shuffling functions
    Added functions to generate and shuffle domino pieces.

--- Jogo_Domino/Dom_JLRV_Model.cpp
+++ Jogo_Domino/Dom_JLRV_Model.cpp
@@ -5,17 +5,42 @@
 //Renato Corral Silva
 //Victor Augusto Toledo Lucio Borghi
 
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>   //necessário pois contem a variavel NULL
 #include "Dom_JLRV_Model.h"
 
-void geradorDomino(peca conjunto[]){
-    int vetor = 0;
-    for (int i = 0; i <= 6; i++){
-        for (int j = i; j <= 6; j++){
-            conjunto[vetor].lado1 = i;
-            conjunto[vetor].lado2 = j;
-            vetor++;
-        }
-    }
+
+//gerar as pecas
+void gerarPecas()
+{
+	int p, i, j;
+	//gerar as 28 pecas
+	p = 0;
+	for (i = 0; i <= 6; i++)
+		for(j = i; j <= 6; j++)
+		 {
+		 	pecas[p].lado1 = i;
+		 	pecas[p].lado2 = j;
+		 	pecas[p].status = NULL;
+		 	p++;
+		 }
+}
+
+//embaralhar as peças
+void embaralharPecas()
+{
+	int i, pos;
+	struct Peca vaux;  //peca auxiliar
+	
+	srand(time(NULL));
+	for (int i = 0; i < 28; i++)
+	  {
+		pos = rand() % 28;   //gera de 0 a 27
+		//troca as pecas
+		vaux = pecas[i];
+		pecas[i] = pecas[pos];
+		pecas[pos] = vaux;
+	  }
+
 }
 
