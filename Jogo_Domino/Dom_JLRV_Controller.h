b15097213c1691f4afa83075da2d0c17b5ba374b
João P.  L. <joaopedrosilva07654@gmail.com>
2026-08-20 11:42:02 -0300

    Refactor Dom_JLRV_Controller.h and add jogar function
    Removed header guards and added 'jogar' function declaration.

--- Jogo_Domino/Dom_JLRV_Controller.h
+++ Jogo_Domino/Dom_JLRV_Controller.h
@@ -4,10 +4,5 @@
 //Leopoldo Ortuzal Zuchieri
 //Renato Corral Silva
 //Victor Augusto Toledo Lucio Borghi
-#ifndef DOM_JLRV_CONTROLLER_H
-#define DOM_JLRV_CONTROLLER_H
-#include "Dom_JLRV_Model.h"
 
-void embaralharConjunto(peca conjunto[]);
-void menu(peca conjunto[]);
-#endif
+void jogar();
