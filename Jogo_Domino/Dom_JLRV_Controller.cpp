0b294aec4650080c866aa5f6b17e6539b77ae2c9
João P.  L. <joaopedrosilva07654@gmail.com>
2026-08-20 11:41:23 -0300

    Refactor game control to use 'jogar' function
    Replaced menu functionality with a new game control function 'jogar' that presents and shuffles pieces multiple times.

--- Jogo_Domino/Dom_JLRV_Controller.cpp
+++ Jogo_Domino/Dom_JLRV_Controller.cpp
@@ -5,43 +5,28 @@
 //Renato Corral Silva
 //Victor Augusto Toledo Lucio Borghi
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "Dom_JLRV_Controller.h"
-#include "Dom_JLRV_View.cpp"
-#include "Dom_JLRV_Model.h"
+#include "Dom_XXXXX_Controller.h"
+#include "Dom_XXXXX_View.cpp"
 
-void menu(peca conjunto[]){
-	int rodando = 1;
-    geradorDomino(conjunto);
-	while(rodando == 1){
-		int escolha;
-		printMenu();
-		scanf("%d", &escolha);
+//aqui está a entrada no jogo e todo o seu controle 
+void jogar()
+{
+
+	apresentarPecas();
+	embaralharPecas();
+	apresentarPecas();
 		
-		if(escolha == 1){
-			embaralharConjunto(conjunto);
-			printarPecas(conjunto);
-		}
-		else if(escolha == 2){
-            geradorDomino(conjunto);
-            printarPecas(conjunto);
-		}
-		else if(escolha == 3){
-            printarPecas(conjunto);
-		}
-        else if(escolha == 4){
-            rodando = 0;
-        }
-	}
-}
+	embaralharPecas();
+	apresentarPecas();
 
+	embaralharPecas();
+	apresentarPecas();
 
-void embaralharConjunto(peca conjunto[]){
-    for(int i = 27; i > 0; i--){ 
-        int j = rand() % (i + 1); 
-        peca temp = conjunto[i]; 
-        conjunto[i] = conjunto[j]; 
-        conjunto[j] = temp; 
-    } 
+	embaralharPecas();
+	apresentarPecas();
+
+	embaralharPecas();
+	apresentarPecas();
+
+	system("pause");
 }
