8e61edc32a5f631f715302f8b94e8f86721ddec6
João P.  L. <joaopedrosilva07654@gmail.com>
2026-08-20 11:40:02 -0300

    Update Dom_JLRV_View.cpp

--- Jogo_Domino/Dom_JLRV_View.cpp
+++ Jogo_Domino/Dom_JLRV_View.cpp
@@ -5,24 +5,17 @@
 //Renato Corral Silva
 //Victor Augusto Toledo Lucio Borghi
 
-#include "Dom_JLRV_View.h"
 #include <stdio.h>
 
-//Menu principal do jogo
-void printMenu(){
-    printf("======Menu=====\n\n");
-	printf("1)Embaralhar pecas\n");
-	printf("2)Desembaralhar pecas\n");
-	printf("3)Mostrar pecas\n");	
-	printf("4)Sair\n");
-	printf("\n===============\n");
-	printf("Escolha uma opcao: ");
-}
+#include "Dom_XXXXX_View.h"
 
-//Funcao responsavel por mostrar as pecas
-void printarPecas(peca conjunto[]){
-    for (int gerar = 0; gerar < 28; gerar++){
-        printf("[%d|%d]", conjunto[gerar].lado1, conjunto[gerar].lado2);
-    }
-    printf("\n\n");
+//SOMENTE AQUI NO VIEW, podemos ter "printf" e "scanf"
+
+//Apresenta todas as peças
+void apresentarPecas()
+{
+	printf("\n");
+	for (int i = 0; i < 28; i++)
+		printf("[%d|%d] ", pecas[i].lado1, pecas[i].lado2);
+	printf("\n\n");
 }
