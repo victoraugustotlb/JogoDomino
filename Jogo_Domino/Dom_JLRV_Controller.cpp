13bfeb74ccb00dfca27fd90373e06d787fc4baa6
João P.  L. <joaopedrosilva07654@gmail.com>
2026-08-26 12:13:41 -0300

    Implement initial piece selection for players
    arrumar a ocasiao que nao ha nenhuma carroca

--- Jogo_Domino/Dom_JLRV_Controller.cpp
+++ Jogo_Domino/Dom_JLRV_Controller.cpp
@@ -37,6 +37,42 @@ void menu(peca conjunto[]){
 }
 
 
+void pecaInicial(peca jogador1[], peca jogador2[], peca *resultadoPeca)
+{
+	int maiorvalorcarroca = -1;
+	peca melhorPeca = {-1, -1};
+	int maiorladojogador1 = 0;
+	int maiorladojogador2 = 0;
+	
+	for(int n = 0; n <= 8; n++)
+		if(jogador1[n].lado1 == jogador1[n].lado2)
+			if (jogador1[n].lado1 > maiorvalorcarroca)
+				{
+				maiorvalorcarroca = jogador1[n].lado1;
+				melhorPeca = jogador1[n];
+				}
+
+		
+	for(int n = 0; n <= 8; n++)
+		if(jogador2[n].lado1 == jogador2[n].lado2)
+			if ( jogador2[n].lado1 > maiorvalorcarroca)
+				{
+				maiorvalorcarroca = jogador2[n].lado2;
+				melhorPeca = jogador2[n];
+				}	
+
+	 if (maiorValorCarroca != -1) 
+	 {
+        *resultadoPeca = melhorPeca;
+	 } 
+	 else 
+	 {
+        resultadoPeca = lado1;
+        resultadoPeca = lado2;
+     }
+	
+}
+
 void embaralharConjunto(peca conjunto[]){
     for(int i = 27; i > 0; i--){ 
         int j = rand() % (i + 1); 
