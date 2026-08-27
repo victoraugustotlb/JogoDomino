71e0d972708fa74aafdeec3eabb7c48a8999f1e0
João P.  L. <joaopedrosilva07654@gmail.com>
2026-08-27 11:23:06 -0300

    Add initial piece selection logic for players

--- Jogo_Domino/Dom_JLRV_Model.cpp
+++ Jogo_Domino/Dom_JLRV_Model.cpp
@@ -19,3 +19,65 @@ void geradorDomino(peca conjunto[]){
     }
 }
 
+void pecaInicial(peca jogador1[], peca jogador2[], peca *resultadoPeca)
+{
+    int maiorvalorcarroca = -1;
+    peca melhorPeca = {-1, -1};
+    
+    int maiorSomaGeral = -1;
+    peca maiorPecaGeral = {-1, -1};
+    
+    // Procura no Jogador 1
+    for(int n = 0; n <= 7; n++) {
+        // Verifica se é carroça
+        if(jogador1[n].lado1 == jogador1[n].lado2) 
+		{
+            if (jogador1[n].lado1 > maiorvalorcarroca) 
+			{
+                maiorvalorcarroca = jogador1[n].lado1;
+                melhorPeca = jogador1[n];
+            }
+        }
+        
+
+        int soma1 = jogador1[n].lado1 + jogador1[n].lado2;
+        if (soma1 > maiorSomaGeral) 
+		{
+            maiorSomaGeral = soma1;
+            maiorPecaGeral = jogador1[n];
+        }
+    							}
+
+
+    for(int n = 0; n <= 7; n++) 
+	{
+        // Verifica se é carroça
+        if(jogador2[n].lado1 == jogador2[n].lado2) 
+		{
+            if (jogador2[n].lado1 > maiorvalorcarroca) 
+			{
+                maiorvalorcarroca = jogador2[n].lado1; 
+                melhorPeca = jogador2[n];
+            }
+        }
+        
+
+        int soma2 = jogador2[n].lado1 + jogador2[n].lado2;
+        if (soma2 > maiorSomaGeral) 
+		{
+            maiorSomaGeral = soma2;
+            maiorPecaGeral = jogador2[n];
+        }
+    }	
+
+    
+    if (maiorvalorcarroca != -1) 
+    {
+        *resultadoPeca = melhorPeca;
+    } 
+    else 
+    {
+        *resultadoPeca = maiorPecaGeral;
+    }
+}
+
